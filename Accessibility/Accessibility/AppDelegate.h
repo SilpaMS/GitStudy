5b4036af83a8d1be739be10285e61bd95ea030bb
SilpaMS <Rapid@Silpas-Mac-mini.local>
2016-04-21 11:31:18 +0530

    First commit

---
+++ Accessibility/Accessibility/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Accessibility
+//
+//  Created by Silpa M S on 2/10/16.
+//  Copyright (c) 2016 RapidValue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
